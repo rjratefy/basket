df564f1888ba21121077339b9ce7d3bf29817916
rjratefy <118510731+rjratefy@users.noreply.github.com>
2023-11-21 10:54:03 +0000

    ha

--- src/Com/Body.js
+++ src/Com/Body.js
@@ -1,33 +1,34 @@
 import '../assets/bootstrap/css/bootstrap.min.css'; 
+import './body.css';
 
 const Body = () => {  
     
-        const corps = {
-            fontFamily:'cursive',
-             marginTop:'30px',
-            //border: '2px solid black', 
-           
-        }; 
-        const produit = {
-             backgroundColor:'yellow',
-             float:'left'
-        };
-        const nav = {
-            backgroundColor:'red',
-            float:'right'
-        };
-    return(
-        <> 
-    <div class='col-md-12' style = {corps} > 
-        <h4> All products </h4>
-        <div class='col-md-8' style = {produit}>
-             fdfd
-        </div>
-        <div class='col-md-4' style = {nav}>
-             fdfdfdf
-        </div>
-    </div>
-        </>
-    );
+        return (
+          <div className="table-container">
+            <table>
+              <thead>
+                <tr>
+                  <th>Nom</th>
+                  <th>Âge</th>
+                  <th>Poste</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>John Doe</td>
+                  <td>30</td>
+                  <td>Développeur</td>
+                </tr>
+                <tr>
+                  <td>Jane Smith</td>
+                  <td>25</td>
+                  <td>Designer</td>
+                </tr>
+                {/* Ajoutez d'autres lignes de données ici */}
+              </tbody>
+            </table>
+          </div>
+        );
+    
 }
 export default Body;
