fdf3bf7ca76749c978376e925912cded0453014c
nix19gremory <chalmaninssa1962002@gmail.com>
2023-11-21 14:54:50 +0100

    front

---
+++ front/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css'; 
+
+import Body from './Com/Body';
+
+function App() {
+  return (
+    <div className="App">
+        <Body> </Body>
+    </div>
+  );
+}
+
+export default App;
---
+++ front/src/Com/Body.js
@@ -0,0 +1,287 @@
+import '../assets/bootstrap/css/bootstrap.min.css'; 
+import './body.css';
+
+const Body = () => {  
+    
+        return (
+          <div className="table-container">
+            <table>
+              <thead>
+                <tr>
+                  <th>Equipe</th>
+                  <th>Nom</th>
+                  <th>Age</th>
+                  <th>Min</th>
+                  <th>Points</th>
+                  <th>Rebond</th>
+                  <th>Assist</th>
+                  <th>Block</th>
+                  <th>Steal</th>
+                  <th>Three throw </th>
+                  <th> Tir </th>
+                  <th>Three Point</th>
+                  
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Celtics</td>
+                  <td>Jason Tatum </td> 
+                  <td>30</td>
+                  <td>25</td>
+                  <td>20</td>
+                  <td>30</td>
+                  <td>10</td>
+                  <td>5</td>
+                  <td>30</td>
+                  <td>40%</td>
+                  <td>20%</td>
+                  <td>30%</td>
+                </tr>
+                <tr>
+                  <td>Celtics</td>
+                  <td>Kemba Walker</td>
+                  <td>30</td>
+                  <td>3</td>
+                  <td>20</td>
+                  <td>30</td>
+                  <td>1</td>
+                  <td>5</td>
+                  <td>30</td>
+                  <td>40%</td>
+                  <td>20%</td>
+                  <td>30%</td>
+                </tr>
+                <tr>
+                  <td>Celtics</td>
+                  <td>Jaylen Brown</td>
+                  <td>24</td>
+                  <td>3</td>
+                  <td>20</td>
+                  <td>30</td>
+                  <td>10</td>
+                  <td>5</td>
+                  <td>30</td>
+                  <td>40%</td>
+                  <td>20%</td>
+                  <td>30%</td>
+                </tr>
+                <tr>
+                  <td>Celtics</td>
+                  <td>Marcus Smart</td>
+                  <td>30</td>
+                  <td>3</td>
+                  <td>20</td>
+                  <td>30</td>
+                  <td>10</td>
+                  <td>5</td>
+                  <td>30</td>
+                  <td>40%</td>
+                  <td>20%</td>
+                  <td>30%</td>
+                </tr>
+                <tr>
+                  <td>Celtics</td>
+                  <td>Robert Williams</td>
+                  <td>30</td>
+                  <td>3</td>
+                  <td>20</td>
+                  <td>30</td>
+                  <td>10</td>
+                  <td>5</td>
+                  <td>30</td>
+                  <td>40%</td>
+                  <td>20%</td>
+                  <td>30%</td>
+                </tr>
+                <tr>
+                  <td>Celtics</td>
+                  <td>Evan Fournier</td>
+                  <td>30</td>
+                  <td>3</td>
+                  <td>20</td>
+                  <td>30</td>
+                  <td>10</td>
+                  <td>5</td>
+                  <td>30</td>
+                  <td>40%</td>
+                  <td>20%</td>
+                  <td>30%</td>
+                </tr>
+                <tr>
+                  <td>Rockets</td>
+                  <td>Russell Westbrook</td>
+                  <td>34</td>
+                  <td>3</td>
+                  <td>20</td>
+                  <td>30</td>
+                  <td>10</td>
+                  <td>5</td>
+                  <td>30</td>
+                  <td>40%</td>
+                  <td>20%</td>
+                  <td>30%</td>
+                </tr>
+                <tr>
+                  <td>Rockets</td>
+                  <td>James Harden</td>
+                  <td>30</td>
+                  <td>3</td>
+                  <td>20</td>
+                  <td>30</td>
+                  <td>10</td>
+                  <td>5</td>
+                  <td>30</td>
+                  <td>40%</td>
+                  <td>20%</td>
+                  <td>30%</td>
+                </tr>
+                <tr>
+                  <td>Rockets</td>
+                  <td>Clint Capela</td>
+                  <td>45</td>
+                  <td>3</td>
+                  <td>20</td>
+                  <td>30</td>
+                  <td>10</td>
+                  <td>5</td>
+                  <td>30</td>
+                  <td>40%</td>
+                  <td>20%</td>
+                  <td>30%</td>
+                </tr>
+                <tr>
+                  <td>Rockets</td>
+                  <td>Eric Gordon</td>
+                  <td>19</td>
+                  <td>3</td>
+                  <td>20</td>
+                  <td>30</td>
+                  <td>10</td>
+                  <td>5</td>
+                  <td>30</td>
+                  <td>40%</td>
+                  <td>20%</td>
+                  <td>30%</td>
+                </tr>
+                <tr>
+                  <td>Rockets</td>
+                  <td>Robert Covington</td>
+                  <td>30</td>
+                  <td>3</td>
+                  <td>20</td>
+                  <td>30</td>
+                  <td>10</td>
+                  <td>5</td>
+                  <td>30</td>
+                  <td>40%</td>
+                  <td>20%</td>
+                  <td>30%</td>
+                </tr>
+                <tr>
+                  <td>Rockets</td>
+                  <td>Austin Rivers </td>
+                  <td>30</td>
+                  <td>3</td>
+                  <td>20</td>
+                  <td>30</td>
+                  <td>10</td>
+                  <td>5</td>
+                  <td>30</td>
+                  <td>40%</td>
+                  <td>20%</td>
+                  <td>30%</td>
+                </tr>
+                <tr>
+                  <td>Heat</td>
+                  <td>Lebron James</td>
+                  <td>27</td>
+                  <td>3</td>
+                  <td>20</td>
+                  <td>30</td>
+                  <td>10</td>
+                  <td>5</td>
+                  <td>30</td>
+                  <td>40%</td>
+                  <td>20%</td>
+                  <td>30%</td>
+                </tr>
+                <tr>
+                  <td>Heat</td>
+                  <td>Dwyane Wade</td>
+                  <td>30</td>
+                  <td>3</td>
+                  <td>20</td>
+                  <td>30</td>
+                  <td>10</td>
+                  <td>5</td>
+                  <td>30</td>
+                  <td>40%</td>
+                  <td>20%</td>
+                  <td>30%</td>
+                </tr>
+                <tr>
+                  <td>Heat</td>
+                  <td>Bam Adebayo</td>
+                  <td>30</td>
+                  <td>3</td>
+                  <td>20</td>
+                  <td>30</td>
+                  <td>10</td>
+                  <td>5</td>
+                  <td>30</td>
+                  <td>40%</td>
+                  <td>20%</td>
+                  <td>30%</td>
+                </tr>
+                <tr>
+                  <td>Heat</td>
+                  <td>Andre Iguodala </td>
+                  <td>30</td>
+                  <td>3</td>
+                  <td>20</td>
+                  <td>30</td>
+                  <td>10</td>
+                  <td>5</td>
+                  <td>30</td>
+                  <td>44%</td>
+                  <td>56%</td>
+                  <td>24%</td>
+                </tr>
+                <tr>
+                  <td>Heat</td>
+                  <td>Tyler Herro</td>
+                  <td>30</td>
+                  <td>3</td>
+                  <td>20</td>
+                  <td>30</td>
+                  <td>10</td>
+                  <td>5</td>
+                  <td>30</td>
+                  <td>10%</td>
+                  <td>23%</td>
+                  <td>34%</td>
+                </tr>
+                <tr>
+                  <td>Heat</td>
+                  <td>Duncan Robinson</td>
+                  <td>30</td>
+                  <td>3</td>
+                  <td>22</td>
+                  <td>30</td>
+                  <td>10</td>
+                  <td>12</td>
+                  <td>30</td>
+                  <td>40%</td>
+                  <td>20%</td>
+                  <td>30%</td>
+                </tr>
+               
+                {/* Ajoutez d'autres lignes de données ici */}
+              </tbody>
+            </table>
+          </div>
+        );
+    
+}
+export default Body;
