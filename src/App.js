f08cc49c7c909cd2c58c24d85e2d68517fddf3eb
rjratefy <118510731+rjratefy@users.noreply.github.com>
2023-11-21 10:42:56 +0000

    test

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 
 import './App.css'; 
 
-import Header from './Com/Header';
 import Body from './Com/Body';
-import Footer from './Com/Footer';
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
-      <Body> </Body>
-      <Footer></Footer>
+        <Body> </Body>
     </div>
   );
 }
